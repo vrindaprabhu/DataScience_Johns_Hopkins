9e8d6fb2bb10c558f4c49fd9de5bb43bb3ec1a26
vrindaprabhu <vrinde91@gmail.com>
2015-12-23 23:20:37 +0530

    Submitting R Programming Assignment 2:23-12-2015

---
+++ R_Programming/Prog_Assign_3/ProgrammingAssignment2.R
@@ -0,0 +1,35 @@
+## Below links have excellent notes and explanation about lexical scoping
+
+#https://asitarrives.wordpress.com/2014/10/18/understanding-lexical-scoping-in-r-great-guidance-for-community-ta-in-coursera/
+#http://adv-r.had.co.nz/Functional-programming.html
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    iessage("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+
+
+
