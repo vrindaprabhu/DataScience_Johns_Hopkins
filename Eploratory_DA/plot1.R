af65571ef684e749ee74064f9f8ec1aa40a9bf93
vrindaprabhu <vrinde91@gmail.com>
2015-12-27 22:39:20 +0530

    Remove wrongly structured directory

--- Eploratory_DA/plot1.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv('household_power_consumption.txt',sep = ';',stringsAsFactors=F)
-
-data.subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-png(file = 'plot1.png', width = 480, height = 480 )
-with(data.subset,hist(as.numeric(Global_active_power), main = 'Global Active Power',
-                      xlab = 'Global Active Power (kilowatts)', col = 'red'))
-dev.off()
