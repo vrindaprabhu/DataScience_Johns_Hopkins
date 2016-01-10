7d4dc5657e16f9bf090664929f82ff490c4a7ddc
vrindaprabhu <vrinde91@gmail.com>
2016-01-11 00:41:42 +0530

    RR : Assignment 1

--- Exploratory_DA/PROJECT_2/exdata-data-NEI_data/plot3.R
+++ Exploratory_DA/PROJECT_2/exdata-data-NEI_data/plot3.R
@@ -16,9 +16,9 @@ types_plot_a <- types_plot_a + geom_line() +
   ylab(expression('Total PM'[2.5]*" Emissions")) +
   ggtitle('Total Emissions in Baltimore City from 1999 to 2008')
 
-png(file = 'plot3a.png', width = 480, height = 480 )
-types_plot_a
-dev.off()
+# png(file = 'plot3a.png', width = 480, height = 480 )
+# types_plot_a
+# dev.off()
 
 PM2_5_Baltimore_Types <- PM2_5_Baltimore[, list(EMISSION=sum(Emissions)), by=key(PM2_5_Baltimore)]
 types_plot_b <- ggplot(PM2_5_Baltimore_Types, aes(as.character(year), EMISSION))
@@ -27,10 +27,10 @@ types_plot_b <- types_plot_b + geom_bar(stat="identity",colour = 'Blue') + facet
   ylab(expression('Total PM'[2.5]*" Emissions")) +
   ggtitle('Total Emissions in Baltimore City from 1999 to 2008')
 
-png(file = 'plot3b.png', width = 480, height = 480 )
-types_plot_b 
-dev.off()
-
+# png(file = 'plot3b.png', width = 480, height = 480 )
+# types_plot_b 
+# dev.off()
+# 
 
 png(file = 'plot3.png', width = 480, height = 480 )
 grid.arrange(types_plot_a,types_plot_b , nrow = 2)
--- Reproducible_Research/Assignment_1/run_PA1_template.R
+++ Reproducible_Research/Assignment_1/run_PA1_template.R
@@ -4,5 +4,4 @@ require(markdown)
 ## Change the working directory to required path
 #setwd("IntroToBDA/Reproducible_Research/")
 
-knit("PA1_template.Rmd", encoding="ISO8859-1")
-markdownToHTML("PA1_template.Rmd", "PA1_template.html")
+knit2html("PA1_template.Rmd", "PA1_template.html")
