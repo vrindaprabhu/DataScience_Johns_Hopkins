4fdf0fa063792ebf649267570d1351d626277f2d
vrindaprabhu <vrinde91@gmail.com>
2016-02-09 00:36:29 +0530

    Cleaning Repo

--- Reproducible_Research/run_PA1_template.R
+++
@@ -1,7 +0,0 @@
-require(knitr)
-require(markdown)
-
-## Change the working directory to required path
-#setwd("IntroToBDA/Reproducible_Research/")
-
-knit2html("PA1_template.Rmd", "PA1_template.html")
